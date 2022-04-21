b30ed50560bae3631cb7868b9f1936d8d2cac937
Nikolaev <n.nikolaevbg97@gmail.com>
2022-04-21 20:43:59 +0300

    Added functions for spliting number list to odd and even numbers in lib_misc module. Second function uses accumolator.

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,15 @@
 % Recreation of the lib_misc module.
 -module(lib_misc).
--export([for/3, perms/1, pythag/1, qsort/1, sum/1]).
+-export([
+		for/3,
+		odds_and_evens/1,
+		odds_and_evens_acc/1,
+		odds_and_evens_acc/3,
+		perms/1,
+		pythag/1,
+		qsort/1,
+		sum/1
+]).
 
 % For loop.
 for(Max, Max, F)
@@ -8,6 +17,26 @@ for(Max, Max, F)
 
 for(I, Max, F)
 	-> [F(I)|for(I+1, Max, F)].
+
+% Splits a list of numbers into 2 lists - even and odd numbers, by traversing the list 2 times.
+odds_and_evens(Numbers) ->
+		Even =  [Num || Num <- Numbers, Num rem 2 =:= 0],
+		Odd = [Num || Num <- Numbers, Num rem 2 =:= 1],
+		{Even, Odd}.
+
+% odds_and_evens with accumolator.
+odds_and_evens_acc(Numbers) ->
+		odds_and_evens_acc(Numbers, [], []).
+
+odds_and_evens_acc([H | T], Odd, Even) ->
+		case H rem 2 of
+				0		-> odds_and_evens_acc(T, Odd, [H | Even]);
+				1		-> odds_and_evens_acc(T, [H | Odd], Even)
+		end;
+		
+odds_and_evens_acc([], Odd, Even) ->
+		{Odd, Even}.
+
 
 % Returns list of permutations from the given string.
 perms([]) -> [[]];
