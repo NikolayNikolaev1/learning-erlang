d34b24a31057e4140d6ea3ab55f5006fc636cdca
Nikolaev <n.nikolaevbg97@gmail.com>
2022-04-21 16:04:19 +0300

    Added permutasion, pythagor and quicksort functions to lib_misc module.

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,6 @@
 % Recreation of the lib_misc module.
 -module(lib_misc).
--export([for/3, sum/1]).
+-export([for/3, perms/1, pythag/1, qsort/1, sum/1]).
 
 % For loop.
 for(Max, Max, F)
@@ -8,6 +8,30 @@ for(Max, Max, F)
 
 for(I, Max, F)
 	-> [F(I)|for(I+1, Max, F)].
+
+% Returns list of permutations from the given string.
+perms([]) -> [[]];
+
+perms(L) 
+	-> [[H | T] || H <- L, T <- perms(L--[H])].
+
+% Take all values of A from 1 to N, all values of B from 1 to N, and all values of C from 1 to N
+% such that A + B + C is less than or equal to N and A*A + B*B = C*C.
+pythag(N)
+	-> [{A, B, C} ||
+		A <- lists:seq(1, N),
+		B <- lists:seq(1, N),
+		C <- lists:seq(1, N),
+		A + B + C =< N,
+		A*A + B*B =:= C*C].
+
+% Quicksorts elements in list from lowest to highest.
+qsort([]) -> [];
+
+qsort([Pivot | T])
+	-> qsort([X || X <- T, X < Pivot])
+		++ [Pivot] ++
+		qsort([X || X <- T, X >= Pivot]).
 
 % Sum a list of numbers and return the result.
 sum(Num)
