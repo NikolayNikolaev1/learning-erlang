fbfddf99d3d80b66cc49fd94a62f443f0cd59852
Nikolaev <n.nikolaevbg97@gmail.com>
2022-04-24 14:11:16 +0300

    Finished homework for lab2. Added rpc to be able to get childrens from main process.

--- labs/server.erl
+++ labs/server.erl
@@ -1,19 +1,32 @@
 -module(server).
--export([loop/1, start/1]).
+-export([createChildren/2, loop/1, start/1, rpc/2]).
+
+createChildren(Pid, Request) ->
+	rpc(Pid, Request).
 
 start({N, Parent}) ->
-	spawn(server, loop, [{N, Parent}]).
+	spawn(server, loop, [{N, Parent, N}]).
 
-loop({0, _Parent}) -> ok;
+loop({0, Parent, _ProcCount}) -> 
+	Parent ! {children, [self()]};
 
-loop({N, Parent}) ->
+loop({N, Parent, ProcCount}) ->
 	receive
-		create_child -> Child = spawn(server, loop, [{N-1, self()}]),
+		create_child -> Child = spawn(server, loop, [{N-1, self(), ProcCount}]),
 				io:format("N:~p, Child:~p~n", [N, Child]),
 				Child ! create_child;
 		{do, X} -> M = X * X,
 			   io:format("Me:~p, N:~p, M:~p~n", [self(), N, M]),
 			   Parent ! {do, M};
+		{children, ChildList} when N =:= ProcCount -> Parent ! ChildList;
+		{children, ChildList} -> Parent ! {children, [self() | ChildList]};
 		Any -> Any
 	end,
-	loop({N, Parent}).
+	loop({N, Parent, ProcCount}).
+
+rpc(Pid, Request) ->
+	Pid ! Request,
+	receive
+		Response ->
+			Response
+	end.
