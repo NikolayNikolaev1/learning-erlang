8aca728c6cd6ae1356c95c5a82ba2a294b7f0e80
Nikolaev <n.nikolaevbg97@gmail.com>
2022-04-30 14:24:24 +0300

    Finished binary tree processes with working changeable functionality. Need to fix race condition and client spaming when returning response of functions result.

--- homeworks/binary_tree.erl
+++ homeworks/binary_tree.erl
@@ -1,49 +1,76 @@
 -module(binary_tree).
--export([createChild/2, loop/1, start/1]).
+-export([client/2, loop/1, rpc/2, start/1]).
 
-createChild(Level, ParentPid) ->
-	spawn(binary_tree, loop, [{Level, ParentPid, [], []}]).
+%% Client commands to change ring server functionallity.
+client(Server, {change_func, multiply_itself}) ->
+	rpc(Server, {change_func, fun(X) -> X*X end});
 
-loop({1, Parent, LChild, RChild}) ->
+client(Server, {change_func, multiplyby_two}) ->
+	rpc(Server, {change_func, fun(X) -> X*2 end});
+
+client(Server, Request) ->
+	rpc(Server, Request).
+
+loop({1, Root, LChild, RChild, {function, F, FuncResult}}) ->
 	Level = 1,
-	State = {Level, Parent, LChild, RChild},
+	Func = {function, F, FuncResult},
+	State = {Level, Root, LChild, RChild, Func},
 
 	receive
-		create_child -> NewState = State;
-		{RootPid, get_tree, TreeList}
+		{Client, {change_func, NewFunc}} -> NewState = {Level, Root, LChild, RChild, {function, NewFunc, []}},
+						    Root ! {Client, func_changed};
+		create_children-> NewState = State;
+		{Client, {exec_func, Input}} -> Result = F(Input),
+						io:format("~p. Server ~p result from function: ~p.~n", [Level, self(), Result]),
+						Root ! {Client, {func_result, Result}},
+						NewState = State;
 		Any -> Any,
 		       NewState = State
 	end,
 
 	loop(NewState);
 
-loop({Level, Parent, LChild, RChild}) ->
-	State = {Level, Parent, LChild, RChild},
+loop({Level, Root, LChild, RChild, {function, F, FuncResult}}) ->
+	Func = {function, F, FuncResult},
+	State = {Level, Root, LChild, RChild, Func},
 
 	receive
-		create_child -> NewLChild = createChild(Level-1, self()),
-				NewRChild = createChild(Level-1, self()),
-				NewLChild ! create_child,
-				NewRChild ! create_child,
-				NewState = {Level, Parent, NewLChild, NewRChild};
-		{get_children, TreeList} -> Parent ! {get_children, [{children, LChild, RChild} | TreeList]},
-					    NewState = State;
-		{RootPid, get_tree, TreeList} -> BinaryTree  = [{Level, self()} | TreeList],
-						 LChild ! {RootPid, get_tree, BinaryTree},
-						 RChild ! {RootPid, get_tree, BinaryTree},
-						 NewState = State;
+		{Client, {change_func, NewFunc}} -> NewState = {Level, Root, LChild, RChild, {function, NewFunc, []}},
+						    LChild ! {Client, {change_func, NewFunc}},
+						    RChild ! {Client, {change_func, NewFunc}};
+		create_children -> NewLChild = spawn(binary_tree, loop, [{Level-1, Root, [], [], Func}]),
+				   NewRChild = spawn(binary_tree, loop, [{Level-1, Root, [], [], Func}]),
+				   NewLChild ! create_children,
+				   NewRChild ! create_children,
+				   NewState = {Level, Root, NewLChild, NewRChild, Func};
+		{Client, {exec_func, Input}} -> Result = F(Input),
+						io:format("~p. Server ~p result from function: ~p.~n", [Level, self(), Result]),
+						LChild ! {Client, {exec_func, Result}},
+						RChild ! {Client, {exec_func, Result}},
+						NewState = {Level, Root, LChild, RChild, {function, F, []}};
+		{Client, func_changed} -> Client ! {self(), func_changed},
+					  NewState = State;
+		{Client, {func_result, Result}} when FuncResult =:= [] -> Response = {func_result, Result},
+									  Client ! {self(), Response},
+									  NewState = {Level, Root, LChild, RChild, {function, F, Result}};
+		set_root -> NewState = {Level, self(), LChild, RChild, Func};
 		Any -> Any,
 		       NewState = State
 	end,
 
 	loop(NewState).
 
-rpc(TreePid, Request, TreeList) ->
-	Tree ! {self(), Request},
+rpc(Server, Request) ->
+	Server ! {self(), Request},
+
 	receive
-		{get_tree, TreeList} -> 
+		{Server, func_changed} -> io:format("Response from server ~p: Function successfully changed.~n", [Server]);
+		{Server, {func_result, Result}} -> io:format("Response from server ~p: ~p.~n", [Server, Result]);
+		Any -> io:format("Any: ~p~n", [Any])
 	end.
 
 start({Size}) ->
-	RootPid = spawn(binary_tree, loop, [{Size, self(), [], []}]),
-	spawn(binary_tree, rpc, [RootPid, [], ]).
+	ServerPid = spawn(binary_tree, loop, [{Size, [], [], [], {function, fun(X) -> X*X end, []}}]),
+	ServerPid ! set_root,
+	ServerPid ! create_children,
+	ServerPid.
